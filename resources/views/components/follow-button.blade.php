deba4b001f851668db3f1fc96558d7ed87ffc16d
Domenico Prestia <domenicoprestiaa@hotmail.com>
2021-03-17 10:38:31 +0100

    Last perfections

--- resources/views/components/follow-button.blade.php
+++
@@ -1,5 +0,0 @@
-<form method="POST" action="/profiles/{{$user->username}}/follow">
-   @csrf
-   <button type="submit" class="bg-green-400 rounded-full py-2 px-2 text-white text-xs ml-2">{{ auth()->user()->following($user) ? 'Unfollow Me' : 'Follow Me' }}
-   </button>
-</form>
--- resources/views/components/input.blade.php
+++
@@ -1,3 +0,0 @@
-@props(['disabled' => false])
-
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50']) !!}>
